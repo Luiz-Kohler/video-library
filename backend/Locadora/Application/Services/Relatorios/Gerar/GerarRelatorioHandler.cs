c1a45bfaf88b20fa7c74cb74d9207637cad7ef71
Luiz-Kohler <luizfelipekohler03@gmail.com>
2022-06-29 23:12:24 -0300

    refac: relatorio

--- backend/Locadora/Application/Services/Relatorios/Gerar/GerarRelatorioHandler.cs
+++ backend/Locadora/Application/Services/Relatorios/Gerar/GerarRelatorioHandler.cs
@@ -1,4 +1,5 @@
-﻿using Domain.IRepositories;
+﻿using Domain.Entities;
+using Domain.IRepositories;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -32,28 +33,6 @@ namespace Application.Services.Relatorios.Gerar
         {
             _logger.LogInformation("Gerando relatorio");
 
-            var clienteComPendenciasAtrasadas = await _clientesRepository.SelecionarVariasPorIncluindoLocacoes(cliente =>
-                cliente.Locacoes.Any(locacao => !locacao.DataDevolucao.HasValue && locacao.EhAtivo) && cliente.EhAtivo);
-
-            var filmesNuncaAlocados = await _filmesRepository.SelecionarVariasPorIncluindoLocacoes(filme =>
-                filme.Locacoes.Count(locacao => locacao.EhAtivo) == 0 && filme.EhAtivo);
-
-            var filmes = await _filmesRepository.SelecionarVariasPorIncluindoLocacoes(filme => filme.EhAtivo);
-
-            var dataAtual = DateTime.UtcNow;
-            var dataUmAnoAtras = dataAtual.AddYears(-1);
-
-            var filmesMaisAlocadosDoUltimoAno = filmes.Where(filme => filme.CriadoEm <= dataAtual && filme.CriadoEm >= dataUmAnoAtras)
-                .OrderByDescending(filme => filme.Locacoes.Count(locacao => locacao.EhAtivo)).ToList();
-
-            var dataUmaSemanaAtras = dataAtual.AddDays(-7);
-
-            var filmesMenosAlocadosSemanaPassada = filmes.Where(filme => filme.CriadoEm <= dataAtual && filme.CriadoEm >= dataUmaSemanaAtras)
-                .OrderByDescending(filme => filme.Locacoes.Count(locacao => locacao.EhAtivo)).ToList();
-
-            var clientes = await _clientesRepository.SelecionarVariasPorIncluindoLocacoes(cliente => cliente.EhAtivo);
-            var segundoClienteQueMaisAlocou = clientes.OrderByDescending(cliente => cliente.Locacoes.Count(locacao => locacao.EhAtivo)).Skip(1).FirstOrDefault();
-
             using (var excelEngine = new ExcelEngine())
             {
                 var application = excelEngine.Excel;
@@ -61,110 +40,15 @@ namespace Application.Services.Relatorios.Gerar
 
                 var xlApp = application.Workbooks.Create();
 
-                var worksheet = xlApp.Worksheets[0];
-                worksheet.Name = "Clientes com pendências";
+                await GerarWorkSheetClientesComPendencias(xlApp);
+                await GerarWorkSheetFilmesNuncaAlocados(xlApp);
 
-                worksheet.Range["A1"].Text = "Id";
-                worksheet.Range["B1"].Text = "Nome";
-                worksheet.Range["C1"].Text = "CPF";
-                worksheet.Range["D1"].Text = "Data Nascimento";
+                var filmes = await _filmesRepository.SelecionarVariasPorIncluindoLocacoes(filme => filme.EhAtivo);
 
-                worksheet.Range["A1:D1"].CellStyle.Font.Bold = true;
-                worksheet.Range["A1:D1"].CellStyle.Color = Color.LightBlue;
+                GerarWorkSheetFilmesMaisAlocadosDoUltimoAno(xlApp, filmes);
+                GerarWorkSheetFilmesMenosAlocadosUltimaSemana(xlApp, filmes);
 
-                var linhaAtual = 2;
-
-                foreach (var cliente in clienteComPendenciasAtrasadas)
-                {
-                    worksheet.Range[$"A{linhaAtual}"].Text = $"{cliente.Id}";
-                    worksheet.Range[$"B{linhaAtual}"].Text = cliente.Nome;
-                    worksheet.Range[$"C{linhaAtual}"].Text = cliente.Cpf;
-                    worksheet.Range[$"D{linhaAtual}"].Text = cliente.DataNascimento.ToLocalTime().ToString("yyyy-MM-dd");
-
-                    linhaAtual++;
-                }
-
-                xlApp.Worksheets.AddCopy(0);
-                var worksheetFilmesNaoAlugados = xlApp.Worksheets[1];
-                worksheetFilmesNaoAlugados.Name = "Filmes Nunca Alocados";
-
-                worksheetFilmesNaoAlugados.Range["A1"].Text = "Id";
-                worksheetFilmesNaoAlugados.Range["B1"].Text = "Titulo";
-                worksheetFilmesNaoAlugados.Range["C1"].Text = "Lançamento";
-
-                worksheetFilmesNaoAlugados.Range["A1:C1"].CellStyle.Font.Bold = true;
-                worksheetFilmesNaoAlugados.Range["A1:C1"].CellStyle.Color = Color.LightBlue;
-
-                linhaAtual = 2;
-
-                foreach (var filme in filmesNuncaAlocados)
-                {
-                    worksheetFilmesNaoAlugados.Range[$"A{linhaAtual}"].Text = $"{filme.Id}";
-                    worksheetFilmesNaoAlugados.Range[$"B{linhaAtual}"].Text = filme.Titulo;
-                    worksheetFilmesNaoAlugados.Range[$"C{linhaAtual}"].Text = filme.EhLancamento ? "Sim" : "Não";
-
-                    linhaAtual++;
-                }
-
-                xlApp.Worksheets.AddCopy(1);
-                var worksheetFilmesMaisAlugados = xlApp.Worksheets[2];
-                worksheetFilmesMaisAlugados.Name = "Top 5 Filmes Mais Alocados Ano Passado";
-
-                worksheetFilmesMaisAlugados.Range["A1"].Text = "Id";
-                worksheetFilmesMaisAlugados.Range["B1"].Text = "Titulo";
-                worksheetFilmesMaisAlugados.Range["C1"].Text = "Lançamento";
-
-                worksheetFilmesMaisAlugados.Range["A1:C1"].CellStyle.Font.Bold = true;
-                worksheetFilmesMaisAlugados.Range["A1:C1"].CellStyle.Color = Color.LightBlue;
-
-                linhaAtual = 2;
-                foreach (var filme in filmesMaisAlocadosDoUltimoAno.Take(5))
-                {
-                    worksheetFilmesMaisAlugados.Range[$"A{linhaAtual}"].Text = $"{filme.Id}";
-                    worksheetFilmesMaisAlugados.Range[$"B{linhaAtual}"].Text = filme.Titulo;
-                    worksheetFilmesMaisAlugados.Range[$"C{linhaAtual}"].Text = filme.EhLancamento ? "Sim" : "Não";
-
-                    linhaAtual++;
-                }
-
-                xlApp.Worksheets.AddCopy(2);
-                var worksheetFilmesMenosAlugados = xlApp.Worksheets[3];
-                worksheetFilmesMenosAlugados.Name = "Top 3 Filmes Menos Alocados Semana Passada";
-
-                worksheetFilmesMenosAlugados.Range["A1"].Text = "Id";
-                worksheetFilmesMenosAlugados.Range["B1"].Text = "Titulo";
-                worksheetFilmesMenosAlugados.Range["C1"].Text = "Lançamento";
-
-                worksheetFilmesMenosAlugados.Range["A1:C1"].CellStyle.Font.Bold = true;
-                worksheetFilmesMenosAlugados.Range["A1:C1"].CellStyle.Color = Color.LightBlue;
-
-                linhaAtual = 2;
-
-                foreach (var filme in filmesMenosAlocadosSemanaPassada.Take(3))
-                {
-                    worksheetFilmesMenosAlugados.Range[$"A{linhaAtual}"].Text = $"{filme.Id}";
-                    worksheetFilmesMenosAlugados.Range[$"B{linhaAtual}"].Text = filme.Titulo;
-                    worksheetFilmesMenosAlugados.Range[$"C{linhaAtual}"].Text = filme.EhLancamento ? "Sim" : "Não";
-
-                    linhaAtual++;
-                }
-
-                xlApp.Worksheets.AddCopy(3);
-                var worksheetSegundoClienteQueMenosAlugou = xlApp.Worksheets[4];
-                worksheetSegundoClienteQueMenosAlugou.Name = "Segundo Cliente Que Mais Alocou filmes";
-
-                worksheetSegundoClienteQueMenosAlugou.Range["A1"].Text = "Id";
-                worksheetSegundoClienteQueMenosAlugou.Range["B1"].Text = "Nome";
-                worksheetSegundoClienteQueMenosAlugou.Range["C1"].Text = "CPF";
-                worksheetSegundoClienteQueMenosAlugou.Range["D1"].Text = "Data Nascimento";
-
-                worksheetSegundoClienteQueMenosAlugou.Range["A1:D1"].CellStyle.Font.Bold = true;
-                worksheetSegundoClienteQueMenosAlugou.Range["A1:D1"].CellStyle.Color = Color.LightBlue;
-
-                worksheetSegundoClienteQueMenosAlugou.Range[$"A2"].Text = $"{segundoClienteQueMaisAlocou?.Id}";
-                worksheetSegundoClienteQueMenosAlugou.Range[$"B2"].Text = segundoClienteQueMaisAlocou?.Nome;
-                worksheetSegundoClienteQueMenosAlugou.Range[$"C2"].Text = segundoClienteQueMaisAlocou?.Cpf;
-                worksheetSegundoClienteQueMenosAlugou.Range[$"D2"].Text = segundoClienteQueMaisAlocou?.DataNascimento.ToLocalTime().ToString("yyyy-MM-dd");
+                await GerarWorkSheetSegundoClienteMaisAlocou(xlApp);
 
                 var stream = new MemoryStream();
                 xlApp.SaveAs(stream);
@@ -179,6 +63,144 @@ namespace Application.Services.Relatorios.Gerar
                     Arquivo = fileStreamResult
                 };
             }
+        }
+
+        private async Task GerarWorkSheetClientesComPendencias(IWorkbook xlApp)
+        {
+            var clienteComPendenciasAtrasadas = await _clientesRepository.SelecionarVariasPorIncluindoLocacoes(cliente =>
+                    cliente.Locacoes.Any(locacao => !locacao.DataDevolucao.HasValue && locacao.EhAtivo) && cliente.EhAtivo);
+
+            var worksheet = xlApp.Worksheets[0];
+            worksheet.Name = "Relatorio";
+
+            worksheet.Range["A1"].Text = "Clientes com pendências";
+
+            worksheet.Range["A2"].Text = "Id";
+            worksheet.Range["B2"].Text = "Nome";
+            worksheet.Range["C2"].Text = "CPF";
+            worksheet.Range["D2"].Text = "Data Nascimento";
+
+            worksheet.Range["A2:D2"].CellStyle.Font.Bold = true;
+            worksheet.Range["A2:D2"].CellStyle.Color = Color.LightBlue;
+
+            var linhaAtual = 3;
+
+            foreach (var cliente in clienteComPendenciasAtrasadas)
+            {
+                worksheet.Range[$"A{linhaAtual}"].Text = $"{cliente.Id}";
+                worksheet.Range[$"B{linhaAtual}"].Text = cliente.Nome;
+                worksheet.Range[$"C{linhaAtual}"].Text = cliente.Cpf;
+                worksheet.Range[$"D{linhaAtual}"].Text = cliente.DataNascimento.ToLocalTime().ToString("yyyy-MM-dd");
+
+                linhaAtual++;
+            }
+        }
+
+        private async Task GerarWorkSheetFilmesNuncaAlocados(IWorkbook xlApp)
+        {
+            var filmesNuncaAlocados = await _filmesRepository.SelecionarVariasPorIncluindoLocacoes(filme =>
+                    filme.Locacoes.Count(locacao => locacao.EhAtivo) == 0 && filme.EhAtivo);
+
+            var worksheet = xlApp.Worksheets[0];
+            worksheet.Range["F1"].Text = "Filmes Nunca Alocados";
+
+            worksheet.Range["F2"].Text = "Id";
+            worksheet.Range["G2"].Text = "Titulo";
+            worksheet.Range["H2"].Text = "Lançamento";
+
+            worksheet.Range["F2:H2"].CellStyle.Font.Bold = true;
+            worksheet.Range["F2:H2"].CellStyle.Color = Color.LightBlue;
+
+            var linhaAtual = 3;
+
+            foreach (var filme in filmesNuncaAlocados)
+            {
+                worksheet.Range[$"F{linhaAtual}"].Text = $"{filme.Id}";
+                worksheet.Range[$"G{linhaAtual}"].Text = filme.Titulo;
+                worksheet.Range[$"H{linhaAtual}"].Text = filme.EhLancamento ? "Sim" : "Não";
+
+                linhaAtual++;
+            }
+        }
+
+        private void GerarWorkSheetFilmesMaisAlocadosDoUltimoAno(IWorkbook xlApp, IList<Filme> filmes)
+        {
+            var dataAtual = DateTime.UtcNow;
+            var dataUmAnoAtras = dataAtual.AddYears(-1);
+
+            var filmesMaisAlocadosDoUltimoAno = filmes.Where(filme => filme.CriadoEm <= dataAtual && filme.CriadoEm >= dataUmAnoAtras)
+                .OrderByDescending(filme => filme.Locacoes.Count(locacao => locacao.EhAtivo)).ToList();
+
+            var worksheet = xlApp.Worksheets[0];
+            worksheet.Range["J1"].Text = "Top 5 Filmes Mais Alocados do ultimo ano";
+
+            worksheet.Range["J2"].Text = "Id";
+            worksheet.Range["K2"].Text = "Titulo";
+            worksheet.Range["L2"].Text = "Lançamento";
+
+            worksheet.Range["J2:L2"].CellStyle.Font.Bold = true;
+            worksheet.Range["J2:L2"].CellStyle.Color = Color.LightBlue;
+
+            var linhaAtual = 3;
+            foreach (var filme in filmesMaisAlocadosDoUltimoAno.Take(5))
+            {
+                worksheet.Range[$"J{linhaAtual}"].Text = $"{filme.Id}";
+                worksheet.Range[$"K{linhaAtual}"].Text = filme.Titulo;
+                worksheet.Range[$"L{linhaAtual}"].Text = filme.EhLancamento ? "Sim" : "Não";
+
+                linhaAtual++;
+            }
+        }        
+        
+        private void GerarWorkSheetFilmesMenosAlocadosUltimaSemana(IWorkbook xlApp, IList<Filme> filmes)
+        {
+            var dataAtual = DateTime.UtcNow;
+            var dataUmaSemanaAtras = dataAtual.AddDays(-7);
+            var filmesMenosAlocadosSemanaPassada = filmes.Where(filme => filme.CriadoEm <= dataAtual && filme.CriadoEm >= dataUmaSemanaAtras)
+                .OrderByDescending(filme => filme.Locacoes.Count(locacao => locacao.EhAtivo)).ToList();
+
+            var worksheet = xlApp.Worksheets[0];
+            worksheet.Range["N1"].Text = "Top 3 Filmes Menos Alocados Semana Passada";
+
+            worksheet.Range["N2"].Text = "Id";
+            worksheet.Range["O2"].Text = "Titulo";
+            worksheet.Range["P2"].Text = "Lançamento";
+
+            worksheet.Range["N2:P2"].CellStyle.Font.Bold = true;
+            worksheet.Range["N2:P2"].CellStyle.Color = Color.LightBlue;
+
+            var linhaAtual = 3;
+
+            foreach (var filme in filmesMenosAlocadosSemanaPassada.Take(3))
+            {
+                worksheet.Range[$"N{linhaAtual}"].Text = $"{filme.Id}";
+                worksheet.Range[$"O{linhaAtual}"].Text = filme.Titulo;
+                worksheet.Range[$"P{linhaAtual}"].Text = filme.EhLancamento ? "Sim" : "Não";
+
+                linhaAtual++;
+            }
+        }        
+        
+        private async Task GerarWorkSheetSegundoClienteMaisAlocou(IWorkbook xlApp)
+        {
+            var clientes = await _clientesRepository.SelecionarVariasPorIncluindoLocacoes(cliente => cliente.EhAtivo);
+            var segundoClienteQueMaisAlocou = clientes.OrderByDescending(cliente => cliente.Locacoes.Count(locacao => locacao.EhAtivo)).Skip(1).FirstOrDefault();
+
+            var worksheet = xlApp.Worksheets[0];
+            worksheet.Range["S1"].Text = "Segundo Cliente Que Mais Alocou filmes";
+
+            worksheet.Range["S2"].Text = "Id";
+            worksheet.Range["T2"].Text = "Nome";
+            worksheet.Range["U2"].Text = "CPF";
+            worksheet.Range["V2"].Text = "Data Nascimento";
+
+            worksheet.Range["S2:V2"].CellStyle.Font.Bold = true;
+            worksheet.Range["S2:V2"].CellStyle.Color = Color.LightBlue;
+
+            worksheet.Range[$"S3"].Text = $"{segundoClienteQueMaisAlocou?.Id}";
+            worksheet.Range[$"T3"].Text = segundoClienteQueMaisAlocou?.Nome;
+            worksheet.Range[$"U3"].Text = segundoClienteQueMaisAlocou?.Cpf;
+            worksheet.Range[$"V3"].Text = segundoClienteQueMaisAlocou?.DataNascimento.ToLocalTime().ToString("yyyy-MM-dd");
         }
     }
 }
